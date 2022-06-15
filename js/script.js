7cbdf24e1560d3a2e05b8aae475a0b358f10621f
syafiqmk <syafiqmuh.k@gmail.com>
2022-06-15 13:03:10 +0800

    tambah section & smooth scroll

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).on('click', 'a[href^="#"]', function(e) {
+    e.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500)
+
+    return false
+})
