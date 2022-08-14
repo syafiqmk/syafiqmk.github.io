6957c421006dde2990c6efdc85108219af78a49d
syafiqmk <syafiqmuh.k@gmail.com>
2022-08-14 12:24:54 +0800

    maintenance

--- js/script.js
+++
@@ -1,9 +0,0 @@
-$(document).on('click', 'a[href^="#"]', function(e) {
-    e.preventDefault();
-
-    $('html, body').animate({
-        scrollTop: $($.attr(this, 'href')).offset().top
-    }, 500)
-
-    return false
-})
